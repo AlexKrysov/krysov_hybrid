e0a03135881b0870600ccad21dc564da71e0c9e6
Alex Krysov <krysov-95@mail.ru>
2023-04-28 10:47:46 +0600

    added test

--- src/test/java/com/krysov/pages/CheckPage.java
+++ src/test/java/com/krysov/pages/CheckPage.java
@@ -2,17 +2,24 @@ package com.krysov.pages;
 
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
 
 public class CheckPage {
 
     private SelenideElement
-            SCALABLE = $(".nav__link.platform__link");
+            SCALABLE = $(".nav__link.platform__link"),
+            features = $(".block.video");
 
     public void checkScalable() {
         step("Проверка заголовка 'SCALABLE'", () -> {
-            SCALABLE.hover();
+            SCALABLE.shouldHave(text("Scalable"));
+        });
+    }
+    public void checkFeatures() {
+        step("Проверка заголовка 'Features'", () -> {
+            SCALABLE.shouldHave(text("Other features"));
         });
     }
 }
--- src/test/java/com/krysov/tests/NavigationTests.java
+++ src/test/java/com/krysov/tests/NavigationTests.java
@@ -1,7 +1,10 @@
 package com.krysov.tests;
 
 import com.codeborne.selenide.Configuration;
+import io.qameta.allure.Owner;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -10,11 +13,24 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class NavigationTests extends TestBase{
 
+    @DisplayName("Проверка наличия заголовка 'SCALABLE' в разделе 'partners'")
+    @Owner("Krysov")
+    @Tag("PositiveNavigationTest")
     @Test
-    void test1() {
+    void checkScalableTest() {
         openPage.openHybridPage();
         navigationPage.hoverPlatform();
         navigationPage.clickPartners();
         checkPage.checkScalable();
+    }
+    @DisplayName("Проверка наличия заголовка 'SCALABLE' в разделе 'partners'")
+    @Owner("Krysov")
+    @Tag("PositiveNavigationTest")
+    @Test
+    void checkFeaturesTest() {
+        openPage.openHybridPage();
+        navigationPage.hoverPlatform();
+        navigationPage.clickPartners();
+        checkPage.checkFeatures();
     }
 }
